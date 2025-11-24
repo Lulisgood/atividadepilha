6dc11bf3261336493f7873faf06d7171a62004be
Lulisgood <136204022+Lulisgood@users.noreply.github.com>
2025-11-23 23:52:14 -0300

    Delete Aula13 - Pilha - LIFO directory

--- Aula13 - Pilha - LIFO/Aula13 - Pilha - LIFO/Livro.py
+++
@@ -1,8 +0,0 @@
-class Livro:
-
-    def __init__(self, titulo, autor ):
-        self.titulo = String
-        self.autor = Autor
-        self.prox = None
-
-        
--- Aula13 - Pilha - LIFO/Livro.py
+++
@@ -1,9 +0,0 @@
-class Livro:
-
-    def __init__(self, titulo, autor, paginas ):
-        self.titulo = titulo
-        self.autor = autor
-        self.paginas = paginas
-        self.prox = None
-
-        
